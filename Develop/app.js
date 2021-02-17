d7da50a2243675c222d5fb9a321afd6e24d8a30b
John Murphy <murphbot55@gmail.com>
2021-02-17 16:39:40 -0500

    adding files

---
+++ Develop/app.js
@@ -0,0 +1,212 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+let newmem;
+let engi;
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
+
+const team = [];
+
+const isName = async (name) => {
+  if (name === "") {
+    return "Must input a name";
+  }
+  return true;
+};
+
+const isNum = async (aNum) => {
+  if (aNum === "") {
+    return "Must input a number";
+  }
+  return true;
+};
+
+// function to validate email...
+
+function main() {
+  // get input from user via inquirer
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "please enter managers name: ",
+        name: "name",
+        //validate: isName(name),
+      },
+      {
+        type: "input",
+        message: "Please enter the managers ID number: ",
+        name: "id",
+        // validate: isNum(id),
+      },
+      {
+        type: "input",
+        message: "Please enter managers email: ",
+        name: "email",
+        //validate: valEmail
+      },
+      {
+        type: "input",
+        message: "Please enter the managers office number: ",
+        name: "officeNumber",
+        //validate: isNum(officeNumber),
+      },
+    ])
+    .then(function (data) {
+      let manager = new Manager(
+        data.name,
+        data.id,
+        data.email,
+        data.officeNumber
+      );
+      team.push(manager);
+      console.log(manager);
+      addMember();
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+async function addMember() {
+  // Inquirer prompt to add new memebers
+  try {
+    let addMem = inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "mems",
+          message: "Would you like to add a team member?",
+          choices: ["YES", "NO"],
+        },
+      ])
+      .then(function (data) {
+        if (data.mems === "YES") {
+          newmem = true;
+        } else {
+          newmem = false;
+        }
+      })
+      .then(function () {
+        if (newmem) {
+          const memType = inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "role",
+                message: "What type of team member would you like to add?",
+                choices: ["ENGINEER", "INTERN"],
+              },
+            ])
+            .then(function (data) {
+              if (data.role === "ENGINEER") {
+                inquirer
+                  .prompt([
+                    {
+                      type: "input",
+                      message: "Enter name of the engineer: ",
+                      name: "name",
+                    },
+                    {
+                      type: "input",
+                      message: "Enter id number of the engineer: ",
+                      name: "id",
+                    },
+                    {
+                      type: "input",
+                      message: "Enter email adress of the engineer: ",
+                      name: "email",
+                    },
+                    {
+                      type: "input",
+                      message: "Enter gitHub accout of engineer: ",
+                      name: "github",
+                    },
+                  ])
+                  .then(function (data) {
+                    let eng = new Engineer(
+                      data.name,
+                      data.id,
+                      data.email,
+                      data.github
+                    );
+                    team.push(eng);
+                    addMember();
+                  });
+              } else {
+                inquirer
+                  .prompt([
+                    {
+                      type: "input",
+                      name: "name",
+                      message: "Please enter the intern name: ",
+                    },
+                    {
+                      type: "input",
+                      name: "id",
+                      message: "Please enter the intern id number: ",
+                    },
+                    {
+                      type: "input",
+                      name: "email",
+                      message: "Please enter the interns email: ",
+                    },
+                    {
+                      type: "input",
+                      name: "school",
+                      message: "Please enter the interns school: ",
+                    },
+                  ])
+                  .then(function (data) {
+                    let bbintern = new Intern(
+                      data.name,
+                      data.id,
+                      data.email,
+                      data.school
+                    );
+                    team.push(bbintern);
+                    addMember();
+                  });
+              }
+            });
+        } else {
+          generateHTML();
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+main();
+
+function generateHTML() {
+  fs.writeFileSync(outputPath, render(team), "utf-8");
+}
---
+++ Readme.md
@@ -0,0 +1,47 @@
+# hmwk9-OOP
+
+# Table of Contents
+
+- [Author](##Author)
+- [Project Description](##Description)
+- [Install](##Install)
+- [Usage](##Usage)
+- [Testing](##Testing)
+- [License](##License)
+- [Contributors](##Contributors)
+- [Questions](##Questions)
+
+## Author
+
+Name: Jack Murphy
+GitHub: jmurphy0
+
+## Description
+
+This project uses node to build a development team via user input.
+
+## Install
+
+insall using npm inquirer, path
+
+## Usage
+
+User must first make a manager, then will be promted to add team members. Either a engineer or an intern will be added to the team using input by user. Output file will contain the HTML output.
+
+## Testing
+
+test this with the test files provided using jest
+
+## License
+
+## Contributors
+
+no
+
+## Questions
+
+no
+
+demo videos are in assets folder of repo
+![alttext](Assets/HMWK9pic.png)
+![alttext](Assets/hmwk9.mp4)
